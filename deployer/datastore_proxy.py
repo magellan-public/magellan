f5b5b690aa8fb4a3e4963ecbac7a32ed3e9b1e86
TonyWang123 <xinwang2014@hotmail.com>
2019-08-09 16:18:12 +0800

    deployer

---
+++ deployer/datastore_proxy.py
@@ -0,0 +1,10 @@
+"""
+This module should connect to datastore to get data e.g., topology
+"""
+
+class datastoreProxy:
+    def __init__(self):
+        pass
+
+    def get_topo(self):
+        pass
