a79f5889e2e620b11a7f1bec3c079fa2b9ac9dae
yutao@yu-T440 <yuta_o@outlook.com>
2019-08-12 14:36:31 +0800

    refactor directory

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from adapter.openflow import OpenFlowAdapter
 
 if __name__ == '__main__':
 
-    #app_dir = 'D:/CZU/PengCheng/magellan-pcl/apps/l2'
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    app_dir = dir_path + '/apps/l2'
-    dp_file = app_dir + '/on_packet.mag'
-    offline_file = app_dir + '/offline.py'
-    out_dir = 'out'
-    ds_file = app_dir + '/ds.json'
+    app_dir = dir_path + '/test/apps/l2/'
+    dp_file = app_dir + 'on_packet.mag'
+    offline_file = app_dir + 'offline.py'
+    ds_file = app_dir + 'ds.json'
+    topo_file = dir_path + '/test/topology/l2.json'
     print(ds_file)
+    # out_dir = 'out'
     portTag, pipeline = compile(dp_file, ds_file)
 
     '''
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         print(str(table.entries))
     '''
 
-    ds = DatastoreProxy('topologies/l2.json')
+    ds = DatastoreProxy(topo_file)
     fg = FlowRulesGenerator(ds)
 
     fg.accept_new_pit(portTag, pipeline)
